d01cc71ab3214cfb8f6aa8883b721ccea7a0ef94
ivan-nev <i_nevakshenov@mail.ru>
2022-07-12 03:07:44 +0500

    1 version

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from pprint import pprint
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        print(response.json())
+        return response.json()
+
+    def upload(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            pprint("Success")
+
+if __name__ == '__main__':
+
+    with open('token.txt', 'r',) as file:
+        token = file.read()
+    print(token)
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file ="/new/test3.txt" # куда загружаем
+    filename = ("test/test.txt")  # что и откуда загружаем
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file, filename)
